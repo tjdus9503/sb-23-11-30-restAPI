307b1a1194402a69c3f8a2b2f7c614769dd8897a
tjdus9503 <tjdus03095@gmail.com>
2023-12-04 18:21:59 +0900

    setting 2, 공통 환경 설정 파일 작성, 각 환경별 설정파일(dev, test) 생성 ..
    dev/test 환경 정상 동작 확인

--- src/test/java/com/ll/sb231130restapi/Sb231130RestApiApplicationTests.java
+++ src/test/java/com/ll/sb231130restapi/Sb231130RestApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.ll.sb231130restapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Sb231130RestApiApplicationTests {
 
     @Test
