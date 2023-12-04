25d27b47cf6a144ddcdce2cd4463651147da524c
tjdus9503 <tjdus03095@gmail.com>
2023-12-04 22:22:48 +0900

    step3, SecurityConfig.java 생성
    정적자원에 접근에 대한 속도 향상을 위해서 우선적으로 시큐리티 룰에서
    허용,
    h2-console 접속할 수 있도록

---
+++ src/main/java/com/ll/sb231130restapi/global/security/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.ll.sb231130restapi.global.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(
+                        authorizeHttpRequests -> authorizeHttpRequests
+                                .requestMatchers(
+                                        PathRequest.toStaticResources().atCommonLocations(), // 성능 향상. 아래 룰을 다 확인하지 않아도 된다.
+                                        new AntPathRequestMatcher("/resources/**"),
+                                        new AntPathRequestMatcher("/h2-console/**")
+                                )
+                                .permitAll()
+                                .requestMatchers(
+                                        "/adm/**"
+                                )
+                                .hasRole("ADMIN")
+                                .anyRequest()
+                                .permitAll()
+                )
+                .headers(
+                        headers -> headers
+                                .addHeaderWriter(
+                                        new XFrameOptionsHeaderWriter(
+                                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)
+                                )
+                )
+                .csrf(
+                        csrf -> csrf
+                                .ignoringRequestMatchers(
+                                        "/h2-console/**"
+                                )
+                )
+                .formLogin(
+                        formLogin -> formLogin
+                                .loginPage("/member/login")
+                )
+                .logout(
+                        logout -> logout
+                                //.logoutUrl("/member/logout") // post 요청만 처리
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout")) // get 요청도 처리
+                )
+        ;
+
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
