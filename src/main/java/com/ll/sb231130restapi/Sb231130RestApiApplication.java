8ef7b3d08fd79918d1d7b9fafa766bde4cdc0767
tjdus9503 <tjdus03095@gmail.com>
2023-12-04 16:48:09 +0900

    setting

---
+++ src/main/java/com/ll/sb231130restapi/Sb231130RestApiApplication.java
@@ -0,0 +1,13 @@
+package com.ll.sb231130restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb231130RestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb231130RestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sb231130restapi/Sb231130RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sb231130restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb231130RestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
