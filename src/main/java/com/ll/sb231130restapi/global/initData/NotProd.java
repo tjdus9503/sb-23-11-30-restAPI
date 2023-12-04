96c40a19375c0964920ed7d2dde2db8e7c845c68
tjdus9503 <tjdus03095@gmail.com>
2023-12-04 23:59:50 +0900

    step 6, 샘플 데이터의 양을 늘린다
    회원 3개, 게시글 10개

--- src/main/java/com/ll/sb231130restapi/global/initData/NotProd.java
+++ src/main/java/com/ll/sb231130restapi/global/initData/NotProd.java
@@ -34,7 +34,18 @@ public class NotProd {
         if (memberService.count() > 0) return;
 
         Member member1 = memberService.join("admin", "1234", "admin@test.com", "관리자").getData();
+        Member member2 = memberService.join("user1", "1234", "user1@test.com", "user1").getData();
+        Member member3 = memberService.join("user2", "1234", "user2@test.com", "user2").getData();
 
-        Article article = articleService.write(member1, "제목1", "내용1").getData();
+        Article article1 = articleService.write(member1, "제목1", "내용1").getData();
+        Article article2 = articleService.write(member1, "제목2", "내용2").getData();
+        Article article3 = articleService.write(member1, "제목3", "내용3").getData();
+        Article article4 = articleService.write(member1, "제목4", "내용4").getData();
+        Article article5 = articleService.write(member2, "제목5", "내용5").getData();
+        Article article6 = articleService.write(member2, "제목6", "내용6").getData();
+        Article article7 = articleService.write(member2, "제목7", "내용7").getData();
+        Article article8 = articleService.write(member2, "제목8", "내용8").getData();
+        Article article9 = articleService.write(member3, "제목9", "내용9").getData();
+        Article article10 = articleService.write(member3, "제목10", "내용10").getData();
     }
 }
