ced13be32bf84ff0a079e84c9049fc18c9bc4f0c
tjdus9503 <tjdus03095@gmail.com>
2023-12-06 10:28:58 +0900

    step 12, GET /api/v1/articles 테스트 케이스 추가

---
+++ src/test/java/com/ll/sb231130restapi/domain/article/article/controller/ApiV1ArticlesControllerTest.java
@@ -0,0 +1,40 @@
+package com.ll.sb231130restapi.domain.article.article.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class ApiV1ArticlesControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("GET /api/v1/articles")
+    void t1() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(get("/api/v1/articles"))
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(handler().handlerType(ApiV1ArticlesController.class))
+                .andExpect(handler().methodName("getArticles"))
+                .andExpect(jsonPath("$.data.items[0].id", is(10)))
+                .andExpect(jsonPath("$.data.items[0].title", is("제목10")));
+    }
+}
