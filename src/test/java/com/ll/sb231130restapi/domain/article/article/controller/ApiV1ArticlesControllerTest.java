3ecc573c5e2d3cc71544a68255d17061037de037
tjdus9503 <tjdus03095@gmail.com>
2023-12-06 10:48:12 +0900

    step 13, GET /api/v1/articles 테스트 케이스 수정, 날짜 데이터
    날짜 관련 데이터는 패턴으로 체크

--- src/test/java/com/ll/sb231130restapi/domain/article/article/controller/ApiV1ArticlesControllerTest.java
+++ src/test/java/com/ll/sb231130restapi/domain/article/article/controller/ApiV1ArticlesControllerTest.java
@@ -8,8 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.matchesPattern;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -17,9 +19,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@Transactional
 public class ApiV1ArticlesControllerTest {
     @Autowired
     private MockMvc mvc;
+
+    // 날짜 패턴 정규식
+    private static final String DATE_PATTERN = "\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.?\\d{0,7}";
 
     @Test
     @DisplayName("GET /api/v1/articles")
@@ -35,6 +41,11 @@ public class ApiV1ArticlesControllerTest {
                 .andExpect(handler().handlerType(ApiV1ArticlesController.class))
                 .andExpect(handler().methodName("getArticles"))
                 .andExpect(jsonPath("$.data.items[0].id", is(10)))
-                .andExpect(jsonPath("$.data.items[0].title", is("제목10")));
+                .andExpect(jsonPath("$.data.items[0].createDate", matchesPattern(DATE_PATTERN)))
+                .andExpect(jsonPath("$.data.items[0].modifyDate", matchesPattern(DATE_PATTERN)))
+                .andExpect(jsonPath("$.data.items[0].authorId", is("3")))
+                .andExpect(jsonPath("$.data.items[0].authorName", is("user2")))
+                .andExpect(jsonPath("$.data.items[0].title", is("제목10")))
+                .andExpect(jsonPath("$.data.items[0].body", is("내용10")));
     }
 }
