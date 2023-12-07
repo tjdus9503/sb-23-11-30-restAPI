5eee01df9145bbcc2c70207a7f5e1947dc1468b6
tjdus9503 <tjdus03095@gmail.com>
2023-12-07 13:48:29 +0900

    step 21, 모든 api 요청(/api/**)이 처리되기 전에 작동하는 필터 도입
    - JwtAuthenticationFilter

--- src/main/java/com/ll/sb231130restapi/global/security/ApiSecurityConfig.java
+++ src/main/java/com/ll/sb231130restapi/global/security/ApiSecurityConfig.java
@@ -7,16 +7,20 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class ApiSecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
         http
                 .securityMatcher("/api/**")
                 .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
         ;
         return http.build();
     }
---
+++ src/main/java/com/ll/sb231130restapi/global/security/JwtAuthenticationFilter.java
@@ -0,0 +1,26 @@
+package com.ll.sb231130restapi.global.security;
+
+import com.ll.sb231130restapi.domain.member.member.service.MemberService;
+import com.ll.sb231130restapi.global.rq.Rq;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final Rq rq;
+    private final MemberService memberService;
+
+    @Override
+    @SneakyThrows
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
+        System.out.println("JwtAuthenticationFilter 실행");
+
+        filterChain.doFilter(request, response);
+    }
+}
