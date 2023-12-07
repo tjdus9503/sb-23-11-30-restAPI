2616f439f129ae864ad72a9bee9fdd850cde799a
tjdus9503 <tjdus03095@gmail.com>
2023-12-07 11:59:43 +0900

    step 19, write 요청에 대한 응답 데이터, 서비스가 리턴한 rs를 기반으로.
    * 신규 RsData.of 메서드 사용 전

--- src/main/java/com/ll/sb231130restapi/domain/article/article/controller/ApiV1ArticlesController.java
+++ src/main/java/com/ll/sb231130restapi/domain/article/article/controller/ApiV1ArticlesController.java
@@ -123,8 +123,12 @@ public class ApiV1ArticlesController {
             @RequestBody WriteArticleRequestBody body
     ) {
         Member member = rq.getMember();
-        Article article = articleService.write(member, body.getTitle(), body.getBody()).getData();
+        RsData<Article> writeRs = articleService.write(member, body.getTitle(), body.getBody());
 
-        return RsData.of("200", "성공", new WriteArticleResponseBody(article));
+        return RsData.of(
+                writeRs.getResultCode(),
+                writeRs.getMsg(),
+                new WriteArticleResponseBody(writeRs.getData())
+        );
     }
 }
